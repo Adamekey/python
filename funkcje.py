fd1231bf64c1892071c018ea19ed901c90db285c
Adamekey <info@extento.pl>
2023-02-21 21:40:27 +0100

    Nauka funkcji

---
+++ funkcje.py
@@ -0,0 +1,4 @@
+# funkcja to kawalek programu, ktory moze przetworzyc dane / argumenty i moze ale nie musi zwrocic na zewnatrz
+# wartosc, ktora mozna wykorzystac w innym miejscu (argument w kolejnych dzialaniach)
+# funkcja moze posiadac wlasne zmienne, uzyteczne tylko dla niej
+# wciecia pokazuja moc przy funkcjach
