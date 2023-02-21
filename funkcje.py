11b85ecd23054e2df7638a7464dfc42425419342
Adamekey <info@extento.pl>
2023-02-21 21:58:24 +0100

    Nauka funkcji cd.

--- funkcje.py
+++ funkcje.py
@@ -2,3 +2,10 @@
 # wartosc, ktora mozna wykorzystac w innym miejscu (argument w kolejnych dzialaniach)
 # funkcja moze posiadac wlasne zmienne, uzyteczne tylko dla niej
 # wciecia pokazuja moc przy funkcjach
+def nazwa_funkcji():  # w nawiasie sa argumenty lub parametry
+    print("Dzialam w funkcji")
+
+print("Dzialam poza funkcja")
+nazwa_funkcji()
+
+# funkcja ktora nic nie zwraca (np. tylko print) - nazywana jest procedura
